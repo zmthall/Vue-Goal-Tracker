bb7af978fe7028ddf90fd61b93297aaa0ea59f09
Zack Thallas <zmthall@ZMT-MacBook-Pro.local>
2025-03-29 00:52:47 -0600

    Removed setup for the makeshift adding of the projects

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,11 @@
 <script setup lang="ts">
 import NoteModal from "./components/note/NoteModal.vue";
 import ProjectDashboard from "./components/project/ProjectDashboard.vue";
-import { useProjectStore } from "./stores/projectStore";
-import Projects from "./assets/Projects.json";
+// import { useProjectStore } from "./stores/projectStore";
+// import Projects from "./assets/Projects.json";
 
-const projectStore = useProjectStore();
-projectStore.projects = Projects;
+// const projectStore = useProjectStore();
+// projectStore.projects = Projects;
 </script>
 
 <style></style>
