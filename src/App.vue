7a8c9d8e04cdf20a69df2956522d818fbfb966de
Zack Thallas <zmthall@ZMT-MacBook-Pro.local>
2025-03-29 13:20:47 -0600

    Made it so the app with instantiate the data one time if the projects is empty.

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,12 @@
 <script setup lang="ts">
 import NoteModal from "./components/note/NoteModal.vue";
 import ProjectDashboard from "./components/project/ProjectDashboard.vue";
-// import { useProjectStore } from "./stores/projectStore";
-// import Projects from "./assets/Projects.json";
+import { useProjectStore } from "./stores/projectStore";
+import Projects from "@/assets/Projects.json";
 
-// const projectStore = useProjectStore();
-// projectStore.projects = Projects;
+const projectStore = useProjectStore();
+
+if (projectStore.projects.length === 0) projectStore.projects = Projects;
 </script>
 
 <style></style>
